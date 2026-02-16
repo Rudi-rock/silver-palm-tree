8ba0c13adcc8329322b713647e1484208008d40d
Developer <dev@oopsbannerapp.com>
2026-02-16 12:51:47 +0530

    UC2: OOPS ASCII art banner using string concatenation with + operator

---
+++ UC2.java
@@ -0,0 +1,25 @@
+/**
+ * UC2: OOPS Banner with ASCII Art
+ * 
+ * Use Case 2 demonstrates ASCII art display:
+ * - Rendering "OOPS" using a 7-line grid of asterisks and spaces
+ * - Manual string concatenation using + operator
+ * - Sequential print statements for precise alignment
+ * - Multi-line console banner formatting
+ * 
+ * @author Developer
+ * @version 1.0
+ */
+public class UC2 {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+        System.out.println();
+        System.out.println(" **** " + " **** " + "***** " + "***** ");
+        System.out.println("*    *" + "*    *" + "*    *" + "*     ");
+        System.out.println("*    *" + "*    *" + "*    *" + "*     ");
+        System.out.println("*    *" + "*    *" + "***** " + "***** ");
+        System.out.println("*    *" + "*    *" + "*     " + "     *");
+        System.out.println("*    *" + "*    *" + "*     " + "     *");
+        System.out.println(" **** " + " **** " + "*     " + "***** ");
+    }
+}
