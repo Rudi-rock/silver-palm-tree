49cead301d3fd79df37caddf203c518929161326
Developer <dev@oopsbannerapp.com>
2026-02-16 12:51:36 +0530

    UC1: Print literal OOPS text - Java fundamentals and entry point

---
+++ UC1.java
@@ -0,0 +1,17 @@
+/**
+ * UC1: Print "OOPS" - Foundational Step
+ * 
+ * Use Case 1 demonstrates Java fundamentals:
+ * - Class as a container for logic
+ * - Main method as the application entry point
+ * - Static keyword and method invocation
+ * - String literals and System.out.println()
+ * 
+ * @author Developer
+ * @version 1.0
+ */
+public class UC1 {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
