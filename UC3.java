b0912ee8a610d7ffbb783b7e3cfab509b9f278fd
Developer <dev@oopsbannerapp.com>
2026-02-16 12:51:57 +0530

    UC3: Refactor OOPS banner using String.join() for memory optimization

--- UC3.java
+++ UC3.java
@@ -1,3 +1,16 @@
+/**
+ * UC3: OOPS Banner with String.join() - Memory Optimization
+ * 
+ * Use Case 3 demonstrates memory-efficient string construction:
+ * - Replaces + operator concatenation with String.join() method
+ * - Reduces intermediate String objects in memory
+ * - Addresses performance issues from string immutability
+ * - Maintains original banner layout with improved efficiency
+ * - Example of using static methods for better memory footprint
+ * 
+ * @author Developer
+ * @version 1.0
+ */
 public class UC3 {
     public static void main(String[] args) {
         System.out.println("OOPS");
